f603be02cb55780bc5f68c8d5c6c133dc8f793c1
Benoit Dossoine <benoit.dossoine@student.ehb.be>
2021-04-01 11:56:42 +0200

    step and run buttons work

--- js/index.js
+++ js/index.js
@@ -1,12 +1,30 @@
 "use strict";
 
+import Game from "./Game.js";
+
+const game = new Game();
 
 function init() {
+    let timer;
+    const stepButton = document.getElementById("step");
+    stepButton.addEventListener("click", function () {
+        game.update();
+        game.render();
+    });
+    const runButton = document.getElementById("run");
+    runButton.addEventListener("click", function () {
+        const timer = setInterval(function () {
+            game.update();
+            game.render();
 
+        }, 1000);
+    });
+    const stopButton = document.getElementById("stop");
+    stopButton.addEventListener("click", function (timer) {
+        console.log("STOP!");
+        clearInterval(timer);
+    });
 }
 
-
 init();
-
-
-
+game.render();
